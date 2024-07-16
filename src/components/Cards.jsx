34c0e42f3b032c96113d1254ca49c764c4ad57e1
saugat-baidwar <saugat.baidwar55@gmail.com>
2024-07-16 12:15:49 +0545

    fetching data from different file and rendering in UI

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,10 +1,18 @@
-const Card = ({ title, image, description, price }) => {
+import products from "../consonants/products";
+
+const Card = () => {
   return (
-    <div className="bg-red-500 text-white p-4">
-      <img src={image} alt="product" className="h-28 rounded-lg" />
-      <h1 className="font-extrabold mt-4">{title}</h1>
-      <p>{description}</p>
-      <p>{price}</p>
+    <div>
+      {products.map((item) => {
+        return (
+          <div className="bg-red-500 text-white p-4" key={item.id}>
+            <img src={item.image} alt="product" className="h-28 rounded-lg" />
+            <h1 className="font-extrabold mt-4">{item.title}</h1>
+            <p>{item.description}</p>
+            <p>{item.price}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
---
+++ src/consonants/products.jsx
@@ -0,0 +1,30 @@
+const products = [
+  {
+    id: 1,
+    title: "Pizza",
+    image: "/biscuts.png",
+    description:
+      "Pizza is a traditional Italian dish typically consisting of a flat base of leavened wheat-based dough topped with tomato, cheese, and other ingredients, Pizza is a traditional Italian dish typically consisting of a flat base of leavened wheat-based dough topped with tomato, cheese, and other ingredients,Pizza is a traditional Italian dish typically consisting of a flat base of leavened wheat-based dough topped with tomato, cheese, and other ingredients,",
+    price: "$ 70",
+    category: "food",
+  },
+  {
+    id: 2,
+    title: "Pizza 1",
+    image: "/biscuts.png",
+    description:
+      "Pizza is a traditional Italian dish typically consisting of a flat base of leavened wheat-based dough topped with tomato, cheese, and other ingredients, Pizza is a traditional Italian dish typically consisting of a flat base of leavened wheat-based dough topped with tomato, cheese, and other ingredients,Pizza is a traditional Italian dish typically consisting of a flat base of leavened wheat-based dough topped with tomato, cheese, and other ingredients,",
+    price: "$ 10",
+    category: "food 2",
+  },
+  {
+    id: 3,
+    title: "Pizza 2",
+    image: "/biscuts.png",
+    description:
+      "Pizza is a traditional Italian dish typically consisting of a flat base of leavened wheat-based dough topped with tomato, cheese, and other ingredients, Pizza is a traditional Italian dish typically consisting of a flat base of leavened wheat-based dough topped with tomato, cheese, and other ingredients,Pizza is a traditional Italian dish typically consisting of a flat base of leavened wheat-based dough topped with tomato, cheese, and other ingredients,",
+    price: "$ 20",
+    category: "food 3",
+  },
+];
+export default products;
